a2cf31e6931c1ee34883c5e8834a56c8c4827376
Fasihagul <sughrabano82@gmail.com>
2024-10-28 20:18:18 +0500

    Add files via upload

---
+++ Js class 13/app.js
@@ -0,0 +1,56 @@
+
+// var divelement=document.getElementById("main")
+// divelement.setAttribute("class","container1")
+// divelement.className="container"
+// console.log(divelement);
+// ///////////////////
+// var button =document.getElementById("btn")
+// btn.setAttribute("class","button")
+// btn.setAttribute("onclick","greet()")
+// console.log(btn.attributes[2].nodeValue)
+
+// console.log(btn.hasAttribute('onmouseover'))
+
+// btn.setAttribute("onclick","greet(this)")
+
+// ////////
+// // creating html
+// var h1element=document.createElement('h1')
+// var h1text=document.createTextNode('hello')
+// console.log(h1element)
+// console.log(h1text);
+// h1element.appendChild(h1text);
+// var dive=document.getElementById('main')
+// dive.appendChild(h1element)
+
+// // ////////
+// // creating link
+// var link=document.createElement("a")
+// var linktext=document.createTextNode('google')
+
+// link.setAttribute('href',"https://www.google.com  ")
+
+// link.appendChild(linktext);
+// var div=document.getElementById('main')
+// div.appendChild(link)
+
+// // creating img
+// var img=document.createElement("img")
+// img.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO8AAxAr9ZNr9JCwn2QZ35rSWWuiayhh0ayQ&s")
+
+// var divee=document.getElementById('main')
+// divee.appendChild(img)
+
+// // createing para
+
+// var para=document.createElement("p")
+// var paratext=document.createTextNode('Humility is the practice of recognizing our limitations and imperfections, and it is essential to personal growth and development. Whether its acknowledging our mistakes and shortcomings,')
+
+
+
+// para.appendChild(paratext);
+
+
+// var diver=document.getElementById('main')
+// diver.appendChild(para)
+
